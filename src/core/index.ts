cae8109f5a84ca7df6bd1747dcd846ca569ead20
z <alex@dwfe.ru>
2022-02-18 02:52:04 +0300

    .

--- src/core/index.ts
+++
@@ -1,6 +0,0 @@
-export {delayAsync, timeout} from './async';
-export {divideWithoutRemainder, isNumber} from './math';
-export {isEqual, IEqualityCheckOpt, isJustObject, isNotJustObject, isPrimitive, isNotPrimitive} from './type';
-export {cloneSimple} from './clone-simple';
-export {Type, IStoppable, TRunMode} from './contract';
-export {rAFQueueLast} from './raf-queue-last';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './core';
+export {delayAsync, timeout} from './core/async';
+export {divideWithoutRemainder, isNumber} from './core/math';
+export {isEqual, IEqualityCheckOpt, isJustObject, isNotJustObject, isPrimitive, isNotPrimitive} from './core/type';
+export {cloneSimple} from './core/clone-simple';
+export {Type, IStoppable, TRunMode} from './core/contract';
+export {rAFQueueLast} from './core/raf-queue-last';
