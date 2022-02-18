ed088aeb871a4daa890eccc08a761f10bb134a01
z <alex@dwfe.ru>
2022-02-18 14:36:42 +0300

    .

--- src/__tests__/is-equal/check/set.ts
+++ src/__tests__/is-equal/check/set.ts
@@ -4,6 +4,7 @@ export function set(): TChecks {
   const s1 = new Set();
   const s2 = new Set();
   const s3 = new Set([1, 2, 3, 4, 5]);
+  const s4 = new Set([1, 2, 7, 4, 5]);
 
   [undefined, null, true, false, -9.35, -7, -0, 0, 1, 7, 9.35, -15n, 0n, 7n, 'hello', '-1', '0', '', '1'].forEach(value => {
     s1.add(value);
@@ -14,5 +15,6 @@ export function set(): TChecks {
     [s1, s2, true],
     [s1, s3, false],
     [s3, s2, false],
+    [s4, s3, false],
   ];
 }
