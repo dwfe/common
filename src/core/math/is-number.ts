1f62558987ebc0fb8651258e98a2efed04020b40
z <alex@dwfe.ru>
2022-02-20 04:53:08 +0300

    .

--- src/core/math/is-number.ts
+++ src/core/math/is-number.ts
@@ -1,6 +1,9 @@
 /**
  * https://stackoverflow.com/questions/18082/validate-decimal-numbers-in-javascript-isnumeric
  */
-export function isNumber(data: any): boolean {
-  return !isNaN(data) && !isNaN(parseFloat(data)) && isFinite(data);
-}
+export const isNumber = (data: any): boolean =>
+  !Number.isNaN(data) &&
+  !Number.isNaN(parseFloat(data))
+  && isFinite(data)
+;
+
