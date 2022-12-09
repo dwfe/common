f81fd253b20937e53a06598cf3b61b06b548e5e8
z <alex@dwfe.ru>
2022-12-10 00:25:53 +0300

    getPropDescriptor

--- src/core/object/getPropDescriptor.ts
+++ src/core/object/getPropDescriptor.ts
@@ -1,15 +1,16 @@
 export function getPropDescriptor(obj: any, prop: string | symbol): PropertyDescriptor {
-  let descriptor = Object.getOwnPropertyDescriptor(obj, prop);
-  if (descriptor) {
-    return descriptor;
-  }
-  descriptor = Object.getOwnPropertyDescriptor(
-    Object.getPrototypeOf(obj), prop
-  );
-  if (!descriptor) {
+  let desc;
+  do {
+    desc = Object.getOwnPropertyDescriptor(obj, prop);
+  } while (
+    !desc &&
+    (obj = Object.getPrototypeOf(obj))
+    );
+
+  if (!desc) {
     const message = `cannot get property descriptor "${String(prop)}"`;
     console.error(message, obj);
     throw new Error(message);
   }
-  return descriptor;
+  return desc;
 }
